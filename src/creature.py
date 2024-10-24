7e34e486549a5ce7f83552b31e2493580b423008
Ilyass <anon>
2024-10-24 13:42:38 +0200

    jjj

--- src/creature.py
+++ src/creature.py
@@ -8,4 +8,12 @@ class Creature:
         self.resistance = resistance  
         self.inventaire = [] 
 
-
+    def __str__(self): #fonction pour afficher
+        """Renvoie une représentation sous forme de chaîne de caractères du personnage."""
+        inventaire_str = ", ".join([arme['nom'] for arme in self.inventaire]) if self.inventaire else "Aucun objet"
+        return (f"Personnage : {self.race}\n"
+                f"Force : {self.force}\n"
+                f"Mana : {self.mana}\n"
+                f"Points de vie : {self.hp}\n"
+                f"Résistance : {self.resistance}\n"
+                f"Inventaire : {inventaire_str}")
