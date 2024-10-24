18e404931265140ff814f253e7fdbcb84139c2e5
Ilyass <anon>
2024-10-24 12:19:10 +0200

    m

--- src/creature.py
+++ src/creature.py
@@ -1,11 +1,11 @@
 class Creature:
     def __init__(self, race: str, force: int, mana: int, hp: int, resistance: int):
+        #Construction de la classe créature
         self.race = race  
-        self.force = force  # 
-        self.mana = mana  
+        self.force = force  
+        self.mana = mana  #mana corresponds à un niveau d'habilité pour pouvoir porter une arme
         self.hp = hp 
         self.resistance = resistance  
         self.inventaire = [] 
 
 
-        
