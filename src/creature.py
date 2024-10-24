83dc45d5cf5c893b95d949306639e122d58dc883
Ilyass <anon>
2024-10-24 12:09:08 +0200

    l

--- src/creature.py
+++ src/creature.py
@@ -1,8 +1,8 @@
 class Creature:
     def __init__(self, race: str, force: int, mana: int, hp: int, resistance: int):
-        self.race = race  # Race du personnage (par ex: Humain, Orc, Elfe)
-        self.force = force  # Force du personnage, affecte les dégâts physiques
-        self.mana = mana  # Mana, capacité à utiliser des armes magiques
-        self.hp = hp  # Points de vie (santé)
-        self.resistance = resistance  # Résistance physique du personnage
-        self.inventaire = []  # Liste d'armes/objets dans l'inventaire
+        self.race = race  
+        self.force = force  # 
+        self.mana = mana  
+        self.hp = hp 
+        self.resistance = resistance  
+        self.inventaire = [] 
