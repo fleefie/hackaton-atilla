b59097e95d01135f1972d0da034cf6b1a6506749
Fleefie <>
2024-10-25 12:49:24 +0200

    awa

--- src/race.py
+++ src/race.py
@@ -1,14 +1,6 @@
+from sorts import Sort
+
 class Race:
-    Type = { 
-        "guerrier": "Noble Guerrier",
-        "elfe": "Elfe Magique",
-        "nain": "Nain Discret",
-        "barbare": "Grand Barbare Combatant",
-        "orc": "Puissant Orc",
-        "erreur": "ERREUR: MAUVAIS TYPE"
-    }
-
-
     """
     Défini une race
 
@@ -17,11 +9,9 @@ class Race:
         description (str): Description courte de la race
         lore (str): Lore de la race
     """
-    def __init__(self, nom: str, description: str, lore: str, type: str):
+    def __init__(self, nom: str, description: str, desclongue: str, stats: dict, sorts: list[Sort]):
         self.nom = nom
         self.description = description
-        self.lore = lore
-        if type in self.Type:
-            self.type = type
-        else:
-            self.type = "erreur"
+        self.desclongue = desclongue
+        self.stats = stats
+        self.sorts = sorts
