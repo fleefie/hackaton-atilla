63d7aba3a32fcb521e46668428a2412b11a54075
Fleefie <>
2024-10-25 12:50:40 +0200

    awawawa

---
+++ src/classes.py
@@ -0,0 +1,17 @@
+from sorts import Sort
+
+class Classe:
+    """
+    Défini une race
+
+    Parameters:
+        nom (str): Nom de la race
+        description (str): Description courte de la race
+        lore (str): Lore de la race
+    """
+    def __init__(self, nom: str, description: str, desclongue: str, stats: dict, sorts: list[Sort]):
+        self.nom = nom
+        self.description = description
+        self.desclongue = desclongue
+        self.stats = stats
+        self.sorts = sorts
